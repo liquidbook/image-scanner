729d1b54c3174c995cf5752b239516a55b5732dc
Felipe Lujan-Bear <felipe@liquidbook.com>
2023-11-08 10:02:29 -0800

    added a leading zero to iamge function

--- src/image-fetcher.js
+++ src/image-fetcher.js
@@ -17,19 +17,27 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('grid-view-toggle').addEventListener('click', function() {
         document.getElementById('imageDisplay').className = 'grid-view';
     });
+
+    createImageModal();
 });
 
+let currentImageIndex = 0;
+let imageSeriesUrls = [];
+
 function handleImageSeries(url) {
-    const urlPattern = url.match(/(.*\[)(\d+)-(\d+)(\].*)/);
+    const urlPattern = url.match(/(.*\[)(0*)(\d+)-(\d+)(\].*)/);
     if (urlPattern) {
         const prefix = urlPattern[1].replace('[', '');
-        const startNum = parseInt(urlPattern[2], 10);
-        const endNum = parseInt(urlPattern[3], 10);
-        const suffix = urlPattern[4].replace(']', '');
+        const leadingZeros = urlPattern[2];
+        const startNum = parseInt(urlPattern[3], 10);
+        const endNum = parseInt(urlPattern[4], 10);
+        const suffix = urlPattern[5].replace(']', '');
 
         let imageUrls = [];
         for (let i = startNum; i <= endNum; i++) {
-            imageUrls.push(`${prefix}${i}${suffix}`);
+            const numStr = leadingZeros + i;
+            const formattedNum = leadingZeros.length > 0 ? numStr.padStart(2, '0') : i.toString();
+            imageUrls.push(`${prefix}${formattedNum}${suffix}`);
         }
         fetchImage(imageUrls);
         displayMessage('Image Series');
@@ -40,12 +48,14 @@ function handleImageSeries(url) {
 
 function fetchImage(urls) {
     const imageDisplayArea = document.getElementById('imageDisplay');
-    imageDisplayArea.innerHTML = ''; // Clear existing images
+    imageDisplayArea.innerHTML = '';
+
+    imageSeriesUrls = urls; // Store the array of image URLs
+
     urls.forEach(url => {
         const image = new Image();
         image.onload = function() {
             displayImage(url, image.naturalWidth, image.naturalHeight);
-            addClickEventToImages(); // Add click event listeners to images for modal functionality
         };
         image.onerror = function() {
             // Handle broken image links by not attempting to display them
@@ -61,9 +71,9 @@ function displayImage(url, width, height) {
 
     const imgElement = document.createElement('img');
     imgElement.src = url;
-    imgElement.className = 'img-fluid'; // Bootstrap class for responsive images
+    imgElement.className = 'img-fluid';
     imgElement.alt = 'Image';
-    
+
     const dimensionDiv = document.createElement('div');
     dimensionDiv.className = 'image-info';
     dimensionDiv.textContent = `${width} x ${height}`;
@@ -71,13 +81,14 @@ function displayImage(url, width, height) {
     containerDiv.appendChild(imgElement);
     containerDiv.appendChild(dimensionDiv);
     imageDisplayArea.appendChild(containerDiv);
+
+    addClickEventToImages();
 }
 
 function displayError(message) {
     const urlError = document.getElementById('urlError');
     urlError.textContent = message;
     urlError.style.color = 'red';
-    addClickEventToImages();
 }
 
 function displayMessage(message) {
@@ -87,7 +98,6 @@ function displayMessage(message) {
 }
 
 function validateUrl(url) {
-    // Simple URL validation function
     const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
@@ -100,25 +110,43 @@ function validateUrl(url) {
 function addClickEventToImages() {
     const images = document.querySelectorAll('#imageDisplay img');
     images.forEach(img => {
-        img.addEventListener('click', function(event) {
-            displayModalImage(event.target.src);
-        });
+        img.removeEventListener('click', handleImageClick); // Remove any existing event to avoid duplicates
+        img.addEventListener('click', handleImageClick);
     });
 }
 
+function handleImageClick(event) {
+    displayModalImage(event.target.src);
+}
+
 function displayModalImage(src) {
-    const modal = document.getElementById('imageModal') || createImageModal();
+    currentImageIndex = imageSeriesUrls.indexOf(src);
+
+    const modal = document.getElementById('imageModal');
     const modalContent = modal.querySelector('.modal-content');
-    modalContent.innerHTML = ''; // Clear existing content
+    modalContent.innerHTML = '';
 
-    const img = document.createElement('img');
+    const img = new Image();
+    img.onload = function() {
+        const filename = src.split('/').pop();
+        const captionText = `${filename} - ${img.naturalWidth} x ${img.naturalHeight}`;
+
+        const captionDiv = document.createElement('div');
+        captionDiv.textContent = captionText;
+        captionDiv.style.textAlign = 'center';
+        captionDiv.style.padding = '10px 0';
+
+        modalContent.appendChild(img);
+        modalContent.appendChild(captionDiv);
+
+        modal.style.display = 'block';
+        modal.classList.add('show');
+    };
     img.src = src;
-    img.style.width = '100%';
-    img.style.height = 'auto';
-    modalContent.appendChild(img);
 
-    modal.style.display = 'block'; // Show the modal
-    modal.classList.add('show'); // Add class to start the animation
+    if (!modal.querySelector('.modal-content')) {
+        modal.appendChild(modalContent);
+    }
 }
 
 function createImageModal() {
@@ -141,7 +169,6 @@ function createImageModal() {
 
     document.body.appendChild(modal);
 
-    // Close modal on clicking the modal background
     modal.addEventListener('click', function(event) {
         if (event.target === modal) {
             modal.classList.remove('show');
@@ -154,14 +181,31 @@ function createImageModal() {
         }
     });
 
-    return modal;
+    // Append navigation arrows to the modal
+    const nextArrow = document.createElement('span');
+    nextArrow.classList.add('material-icons');
+    nextArrow.textContent = 'navigate_next';
+    nextArrow.onclick = navigateNext;
 
-    // Listen for the end of the transition to set display to none
-    modal.addEventListener('transitionend', function(event) {
-        if (event.propertyName === 'opacity' && !modal.classList.contains('show')) {
-            modal.style.display = 'none';
-        }
-    });
+    const prevArrow = document.createElement('span');
+    prevArrow.classList.add('material-icons');
+    prevArrow.textContent = 'navigate_before';
+    prevArrow.onclick = navigatePrevious;
+
+    modalContent.appendChild(prevArrow);
+    modalContent.appendChild(nextArrow);
+
+    return modal;
+}
+
+function navigateNext() {
+    currentImageIndex = (currentImageIndex + 1) % imageSeriesUrls.length;
+    displayModalImage(imageSeriesUrls[currentImageIndex]);
+}
+
+function navigatePrevious() {
+    currentImageIndex = (currentImageIndex - 1 + imageSeriesUrls.length) % imageSeriesUrls.length;
+    displayModalImage(imageSeriesUrls[currentImageIndex]);
 }
 
 window.addEventListener('click', function(event) {
